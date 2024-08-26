8c27e761e0997845a1ba61b4658cf08a6313894e
raghib01 <rakibulislamragib2@gmail.com>
2024-08-26 18:34:41 +0600

    using react-router-dom create navigation routes

--- Blog_frontend/src/App.jsx
+++ Blog_frontend/src/App.jsx
@@ -1,4 +1,12 @@
 import React from 'react'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { Home } from './Pages/Home'
+import { About } from './Pages/About'
+import { Service } from './Pages/Service'
+import { Projects } from './Pages/Projects'
+import { DashBoard } from './Pages/DashBoard'
+import { SignIn } from './Pages/SignIn'
+import { SignUp } from './Pages/SignUp'
 
 export const App = () => {
   return (
