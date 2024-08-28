144e49171ba4077437a01de301f2fd78b550ec08
raghib01 <rakibulislamragib2@gmail.com>
2024-08-28 18:27:14 +0600

    connect with database with insomnia, sign in api routes

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,3 +1,35 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+
 export const signup = async (req, res) => {
-  console.log(req.body);
+  const { username, email, password } = req.body;
+
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === "" ||
+    email === "" ||
+    password === ""
+  ) {
+    return res.status(400).json({ error: "All fields are required" });
+  }
+
+  const hashPass = bcryptjs.hashSync(password, 10);
+
+  const newUser = new User({
+    username,
+    email,
+    password: hashPass,
+  });
+
+  try {
+    await newUser.save();
+    res.json({ message: "User registered successfully" });
+  } catch (error) {
+    res.status(500).json({
+      error: "Error registering user",
+      details: error.message,
+    });
+  }
 };
